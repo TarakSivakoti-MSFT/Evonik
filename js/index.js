333a362e3f03f8ce294c15315586502dba506872
TarakSivakoti <v-sivsat@microsoft.com>
2020-02-27 20:47:36 +0530

    added new banner and css

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
-var http = require("http");
-http
-  .createServer(function(req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(req.url);
-    res.end();
-  })
-  .listen(8080);
+const http = require("http");
+
+// Create an instance of the http server to handle HTTP requests
+let app = http.createServer((req, res) => {
+  // Set a response type of plain text for the response
+  res.writeHead(200, { "Content-Type": "text/plain" });
+
+  // Send back a response and end the connection
+  res.end("Hello World!\n");
+});
+
+// Start the server on port 3000
+app.listen(3000, "127.0.0.1");
+console.log("Node server running on port 3000");
